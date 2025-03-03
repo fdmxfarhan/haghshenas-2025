f56948d3800c9cadee7d966462640d3d72fe03ab
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-03-03 16:03:39 +0330

    basic move

---
+++ 01 HSPS basic/controllers/rcj_soccer_team_blue/robot3.py
@@ -0,0 +1,15 @@
+import math
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+from math import *
+from utils import *
+
+class MyRobot3(RCJSoccerRobot):
+    def run(self):
+        tarif_moteghayer(self)
+        while self.robot.step(TIME_STEP) != -1: # تا زمانی که بازی در حال اجراست
+            if self.is_new_data(): # اگر دیتای جدیدی آماده خواندن بود
+                readData(self)
+                if self.is_ball:
+                    move(self, self.xb , self.yb)
+                else:
+                    move(self, 0, -0.5)
---
+++ 01 HSPS basic/controllers/rcj_soccer_team_yellow/robot1.py
@@ -0,0 +1,14 @@
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+from math import *
+from utils import *
+
+class MyRobot1(RCJSoccerRobot):
+    def run(self):
+        tarif_moteghayer(self)
+        while self.robot.step(TIME_STEP) != -1: # تا زمانی که بازی در حال اجراست
+            if self.is_new_data(): # اگر دیتای جدیدی آماده خواندن بود
+                readData(self)
+                if self.is_ball:
+                    move(self, self.xb , self.yb)
+                else:
+                    formation(self)
